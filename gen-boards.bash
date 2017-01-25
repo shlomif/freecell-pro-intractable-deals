23ecf125d6d2c42934999201801c6b1e7f663183
Shlomi Fish <shlomif@shlomifish.org>
2017-01-25 18:00:03 +0200

    Remove trailing whitespace.

--- gen-boards.bash
+++ gen-boards.bash
@@ -3,5 +3,5 @@ if ! test -d deals ; then
     mkdir deals
 fi
 for i in `cat intractable.txt` ; do
-    pi-make-microsoft-freecell-board -t "$i" > deals/"$i".board
+    pi-make-microsoft-freecell-board -t "$i" | perl -lape 's/[ \t]+$//' > deals/"$i".board
 done
